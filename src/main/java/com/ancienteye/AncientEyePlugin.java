ea96128cf17cbc74a18e38c253f940ac5cfc7af7
Easy00i <srikantosarkar077@gmail.com>
2026-03-19 11:45:08 +0530

    Update AncientEyePlugin.java

--- src/main/java/com/ancienteye/AncientEyePlugin.java
+++ src/main/java/com/ancienteye/AncientEyePlugin.java
@@ -16,36 +16,32 @@ public class AncientEyePlugin extends JavaPlugin {
     public void onEnable() {
         instance = this;
         
-        // 1. Initialize Managers (Pass 'this' to satisfy the constructors)
+        // 1. Initialize Managers
         this.playerDataManager = new PlayerDataManager(this);
         this.cooldownManager = new CooldownManager(this);
         this.abilityLogic = new AbilityLogic(this);
         this.animationTradeManager = new AnimationTradeManager(this);
         this.eventManager = new EventManager(this);
-        
-        // CommandManager usually needs the plugin instance too
         this.commandManager = new CommandManager(this);
 
-        // 2. Register Listeners (Pass necessary instances to the triggers)
-        getServer().getPluginManager().registerEvents(new AbilityTrigger(this, abilityLogic), this);
+        // 2. Register Listeners 
+        // FIX: AbilityTrigger class only needs 'this' as per your constructor
+        getServer().getPluginManager().registerEvents(new AbilityTrigger(this), this);
         getServer().getPluginManager().registerEvents(eventManager, this);
         getServer().getPluginManager().registerEvents(abilityLogic, this); 
 
         // 3. Register Commands
-        getCommand("smpstart").setExecutor(commandManager);
-        getCommand("eye").setExecutor(commandManager);
-        getCommand("trade").setExecutor(commandManager);
-        getCommand("event").setExecutor(commandManager);
+        if (getCommand("smpstart") != null) getCommand("smpstart").setExecutor(commandManager);
+        if (getCommand("eye") != null) getCommand("eye").setExecutor(commandManager);
+        if (getCommand("trade") != null) getCommand("trade").setExecutor(commandManager);
+        if (getCommand("event") != null) getCommand("event").setExecutor(commandManager);
 
         getLogger().info("Ancient Eye SMP Plugin Loaded Successfully!");
     }
 
     public static AncientEyePlugin get() { return instance; }
     public PlayerDataManager getPlayerData() { return playerDataManager; }
-    
-    // Compatibility method if other classes use 'getDataManager'
     public PlayerDataManager getDataManager() { return playerDataManager; }
-    
     public CooldownManager getCooldownManager() { return cooldownManager; }
     public AbilityLogic getAbilityLogic() { return abilityLogic; }
     public AnimationTradeManager getTradeManager() { return animationTradeManager; }
