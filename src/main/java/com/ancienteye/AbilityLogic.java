3d8eeebdd9c320b14d0566b496fc2a8372cd2c71
Easy00i <srikantosarkar077@gmail.com>
2026-03-24 14:01:38 +0530

    Update AbilityLogic.java

--- src/main/java/com/ancienteye/AbilityLogic.java
+++ src/main/java/com/ancienteye/AbilityLogic.java
@@ -13,6 +13,8 @@ import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
